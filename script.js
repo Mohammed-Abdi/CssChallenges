14b024c3cd9578e1cb9575a4e12b70905449f427
Mohammed <mammegentleman@gmail.com>
2025-03-10 21:31:24 +0300

    updated

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const whatIsThis = document.querySelector(".whatIsThis");
 const popUp = document.querySelector(".popUpDetail");
+const main = document.querySelector("main");
 
 whatIsThis.addEventListener("click", () => {
   popUp.classList.toggle("show");
+});
+
+main.addEventListener("click", () => {
+  popUp.classList.remove("show");
 });
